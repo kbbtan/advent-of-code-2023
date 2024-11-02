c571e6ee2d666bd445ebd06dceda8ed1aaacca98
Kian Boon <kbbbtan@gmail.com>
2024-11-02 02:36:20 -0700

    Added templating

---
+++ template/solution.py
@@ -0,0 +1,23 @@
+"""
+Contains solutions for Day N stars.
+Run on Python 3.8.3.
+"""
+
+INPUT_FILE = "input.txt"
+
+def star_1():
+    """ Solution for Star 1.
+    """
+    
+def star_2():
+    """ Solution for Star 2.
+    """
+    
+    
+def main():
+    """ Contains driver code for running solutions.
+    """
+    print("Solutsion for Star 1: ", star_1())
+    print("Solution for Star 2: ", star_2())
+
+main()
